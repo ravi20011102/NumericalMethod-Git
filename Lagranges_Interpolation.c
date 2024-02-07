559531129a9fa21a5bb432f9451ef03842d79fcb
ravi31061 <137847883+ravi31061@users.noreply.github.com>
2024-02-07 09:39:02 +0545

    first commit

---
+++ Lagranges_Interpolation.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int n, i, j;
+    float x[10], fx[10], x1, sum = 0.0;
+    char ch;
+    printf("Enter the no. of terms n : ");
+    scanf("%d", &n);
+    printf("Enter the respective values of x0 to xn and f0 to fn : \n");
+    for (i = 0; i < n; i++){
+        printf("x%d = ", i);
+        scanf("%f", &x[i]);
+        printf("f(%.1f) = ", x[i]);
+        scanf("%f", &fx[i]);
+    }
+    printf("Enter the value of x to find the respective function value : ");
+    scanf("%f", &x1);
+    for (i = 0; i < n; i++){
+        float p = 1;
+        for (j = 0; j < n; j++){
+            if (j != i){
+                p = p * ((x1 - x[j]) / (x[i] - x[j]));
+            }
+        }
+        sum = sum + (fx[i] * p);
+    }
+    printf("f(%.2f) : %.2f\n", x1, sum);
+    return 0;
+}
